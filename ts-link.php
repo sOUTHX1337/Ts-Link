13734e9c164a60396a0851765c3b60a13f2cf1cc
sOUTHX <git@southx.net>
2017-10-26 20:22:57 +0200

    Cleaning up
    Cleaning up

--- ts-link.php
+++ ts-link.php
@@ -14,7 +14,7 @@
 		$ts3_VirtualServer = TeamSpeak3::factory("serverquery://".$server["ts_query_admin"].":".$server["ts_query_password"]."@".$server["tsip"].":".$server["ts_query_port"]."/?server_port=".$server["tsport"]."&nickname=".$server["ts_query_user_nick"]."");
 		
 		$client = $ts3_VirtualServer->clientFindDb($_POST["Inhalt"], true);
-		if( $ts3_VirtualServer->serverGroupClientAdd(Gruppe, $client[0]) )
+		if( $ts3_VirtualServer->serverGroupClientAdd(GRUPPE, $client[0]) )
 			echo "Dein Rank wurde erfolgreich geändert!";
 		
 	} catch(Exception $e) {
